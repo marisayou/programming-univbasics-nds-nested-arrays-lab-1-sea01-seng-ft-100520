f8c035473902885896f43cbdbfe75ce6392f32a7
Marisa You <marisa.you@gmail.com>
2020-08-31 19:52:18 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -22,7 +22,7 @@ def assembled_matrix
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
   # Make sure conventional produce is first, on the 'zeroth' / 'bottom' shelf
   
-  return [COVENTIONAL_PRODUCE, ORGANIC_PRODUCS]
+  return [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCS]
 end
 
 def sorted_matrix
